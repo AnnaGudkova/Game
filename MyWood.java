d1f819a6a7c6373c937aef001719fd6c232153ac
AnnaGudkova <anyagud@rambler.ru>
2014-03-11 14:53:02 +0600

    Update MyWood.java

--- MyWood.java
+++ MyWood.java
@@ -1,10 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MyWood implements Wood {
 	private static String m_name;
 	private static Point m_start;
-	MyWoodman m_woodmanList;
+	MyWoodman m_woodman;
+	Map <String,MyWoodman> m_woodmanList=new HashMap<String,MyWoodman>();
 	private char [][] m_wood;
-
+	
 	public MyWood(char[][] wood) {
 		m_wood=wood;
 	}
@@ -12,110 +16,101 @@ public class MyWood implements Wood {
 	public void createWoodman(String name, Point start) {
 		m_name = name;
 		m_start=start;
-		m_woodmanList=new MyWoodman(m_name,m_start);
+		m_woodman=new MyWoodman(m_name,m_start);
+		m_woodmanList.put(m_name,m_woodman);		
 	}
 
 	public Action move(String name, Direction direction) {
 		Action result=null;
-		String new_name;
-		new_name=m_woodmanList.GetName();
-		if (name!=new_name) {
+		MyWoodman nn=m_woodmanList.get(m_name);
+		if (name!=nn.GetName()) {
 			result=Action.WoodmanNotFound;
 		} else {
-			if(m_woodmanList.GetLifeCount()>-1) {
-				if (direction==Direction.Up) {//Point k=m_woodmanList.GetLocation();
+			if(m_woodman.GetLifeCount()>-1) {
+				if (direction==Direction.Up) {
 					Point n=new Point(m_start.getX()-1,m_start.getY());
 					if (m_wood[m_start.getX()-1][m_start.getY()]=='K') {
-						m_woodmanList.Kill();
+						m_woodman.Kill();
 						result=Action.Dead;
-
 					}
 					if (m_wood[m_start.getX()-1][m_start.getY()]=='1') {
-						m_woodmanList.SetLocation(n); 
 						result=Action.Fail;
 					}
 					if (m_wood[m_start.getX()-1][m_start.getY()]=='0') {
-
 						result=Action.Ok;
-						m_woodmanList.SetLocation(n); 
+						m_woodman.SetLocation(n); 
 					}
 					if (m_wood[m_start.getX()-1][m_start.getY()]=='L') {
-						m_woodmanList.AddLife();
+						m_woodman.AddLife();
 						result=Action.Life;
+						m_woodman.SetLocation(n); 
 					}
 				}
 
 				if (direction==Direction.Down) {
-					//m_wood[m_start.getX()][m_start.getY()]=m_wood[m_start.getX()+1][m_start.getY()]; 
 					Point n=new Point(m_start.getX()+1,m_start.getY());
 					if (m_wood[m_start.getX()+1][m_start.getY()]=='K') {
-						m_woodmanList.Kill();
+						m_woodman.Kill();
 						result=Action.Dead;
-
 					}
-					if (m_wood[m_start.getX()+1][m_start.getY()]=='1') {
-						m_woodmanList.SetLocation(n); 
+					if (m_wood[m_start.getX()+1][m_start.getY()]=='1') { 
 						result=Action.Fail;
 					}
 					if (m_wood[m_start.getX()+1][m_start.getY()]=='0') {
-
 						result=Action.Ok;
-						m_woodmanList.SetLocation(n); 
+						m_woodman.SetLocation(n); 
 					}
 					if (m_wood[m_start.getX()+1][m_start.getY()]=='L') {
-						m_woodmanList.AddLife();
+						m_woodman.AddLife();
 						result=Action.Life;
 					}
 				}
 
 				if (direction==Direction.Left) {
 					Point n=new Point(m_start.getX(),m_start.getY()-1);
-					//m_wood[m_start.getX()][m_start.getY()]=m_wood[m_start.getX()][m_start.getY()-1]; 
 					if (m_wood[m_start.getX()][m_start.getY()-1]=='K') {
-						m_woodmanList.Kill();
+						m_woodman.Kill();
 						result=Action.Dead;
 
 					}
 					if (m_wood[m_start.getX()][m_start.getY()-1]=='1') {
-						m_woodmanList.SetLocation(n); 
 						result=Action.Fail;
 					}
 					if (m_wood[m_start.getX()][m_start.getY()-1]=='0') {
-						m_woodmanList.SetLocation(n); 
+						m_woodman.SetLocation(n); 
 						result=Action.Ok;
 					}
 					if (m_wood[m_start.getX()][m_start.getY()-1]=='L') {
-						m_woodmanList.AddLife();
+						m_woodman.AddLife();
 						result=Action.Life;
+						m_woodman.SetLocation(n); 
 					}
 				}
 
 				if (direction==Direction.Right) {
 					Point n=new Point(m_start.getX(),m_start.getY()+1);
-					//m_wood[m_start.getX()][m_start.getY()]=m_wood[m_start.getX()][m_start.getY()+1]; 
-
 					if (m_wood[m_start.getX()][m_start.getY()+1]=='K') {
-						m_woodmanList.Kill();
+						m_woodman.Kill();
 						result=Action.Dead;
 
 					}
 					if (m_wood[m_start.getX()][m_start.getY()+1]=='1') {
-						m_woodmanList.SetLocation(n); 
 						result=Action.Fail;
 					}
 					if (m_wood[m_start.getX()][m_start.getY()+1]=='0') {
-						m_woodmanList.SetLocation(n); 
+						m_woodman.SetLocation(n); 
 						result=Action.Ok;
 					}
 					if (m_wood[m_start.getX()][m_start.getY()+1]=='L') {
-						m_woodmanList.AddLife();
+						m_woodman.AddLife();
 						result=Action.Life;
+						m_woodman.SetLocation(n); 
 					}
 				} 			
 				if (direction==Direction.None) {
 
 					if (m_wood[m_start.getX()][m_start.getY()]=='K') {
-						m_woodmanList.Kill();
+						m_woodman.Kill();
 						result=Action.Dead;
 
 
@@ -127,8 +122,9 @@ public class MyWood implements Wood {
 						result=Action.Ok;
 					}
 					if (m_wood[m_start.getX()][m_start.getY()]=='L') {
-						m_woodmanList.AddLife();
+						m_woodman.AddLife();
 						result=Action.Life;
+						
 					}
 				}	
 			} else
