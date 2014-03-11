9d4e0a04fe412e78153c1fb0ae7c22f81dcfd40c
AnnaGudkova <anyagud@rambler.ru>
2014-03-11 21:09:16 +0600

    Update MyWoodLoader.java

--- MyWoodLoader.java
+++ MyWoodLoader.java
@@ -10,30 +10,31 @@ public class MyWoodLoader implements WoodLoader {
 	private char[][] m_wood;
 	MyWood world=new MyWood(m_wood);	
 
-		public Wood Load(InputStream stream) throws IOException, CodeException {
+	public Wood Load(InputStream stream) throws IOException, CodeException {
 		int n=0;
 		int m=0;
 		List<String> list = new LinkedList<String>();
 		Scanner sc= new Scanner(new InputStreamReader(stream,"utf8"));
 		try {
-			n=sc.nextLine().length();
-			list.add(sc.toString());
-			while(sc.hasNextLine()) { {
+			list.add(sc.nextLine());
+			m=1;
+			while(sc.hasNextLine()) { 			
 				list.add(sc.nextLine());
-				m++;
+				m++;				
 			}
-				if(sc.nextLine().length()==n){
-					m_wood=new char[n][m];
-					for (int i=0;i<m;i++){
-						
-						m_wood[i]=list.get(i).toCharArray();	
-						}
-				} else throw new CodeException("Input correct world!");
-			}
+			n=list.get(1).length();
+			for (int j=0;j<m;j++)
+			{
+				if (list.get(j).length()!=n) throw new CodeException("Input correct world");
+				else
+					m_wood=new char[m][n];
+				for (int i=0;i<m;i++){
+					m_wood[i]=list.get(i).toCharArray();	
+				}			
+			}				
 		} finally	{
 			sc.close();	
 		}
-			return world;
+		return world;
 	}
 }
-
